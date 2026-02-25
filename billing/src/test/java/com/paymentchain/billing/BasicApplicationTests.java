e21223f60c5d627891b79f47084e230bf26d4487
Chuyiti <iti.arizmendi@gmail.com>
2026-02-24 23:54:52 -0600

    add test chuy2

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -10,7 +10,7 @@ public class BasicApplicationTests  {
 
     @Test
     void contextLoads() {
-       String message = "Default Message Cambio Test Chuy";
+       String message = "Default Message Cambio Test Chuy prueba 2";
         assertNotNull(message);
     }
 }
